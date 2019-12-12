cd5d66db5e770393670a960f1636037003c40398
Mustafa Muse <umpp101@icloud.com>
2019-12-11 16:40:19 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const todoForm = document.querySelector("#create-task-form");
+
+todoForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const newTaskDescription = event.target.querySelector(
+    "#new-task-description"
+  );
+  const taskList = document.querySelector("#tasks");
+
+  const newTask = document.createElement("p");
+  newTask.textContent = newTaskDescription.value;
+  taskList.appendChild(newTask);
+
+  const deleteSign = document.createElement("button");
+  deleteSign.textContent = "Finished";
+  deleteSign.classList.add("button");
+  newTask.appendChild(deleteSign);
+});
+
+document.addEventListener("click", function (event) {
+  if (event.target.classList.contains("button"))
+    event.target.parentElement.remove();
 });
